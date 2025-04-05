e5314e154c606facfff8dbdb734b65483421a324
Choi-Eunseok <ted12333@gmail.com>
2025-04-05 23:41:26 +0900

    fix: update detail change to main

--- backend/src/routers/activity/[id]/update/details.ts
+++ backend/src/routers/activity/[id]/update/details.ts
@@ -8,15 +8,15 @@ const details = new Elysia()
   .use(activityAuthorityService())
   .use(ActivityModel)
   .patch(
-    "/details",
+    "details",
     async ({ body, activity, activityModel, error }) => {
-      const updateData = {
-        ...body,
-      };
-
       const updated = await activityModel.db.updateOne(
         { _id: activity._id },
-        { $set: updateData },
+        {
+          $set: {
+            ...body,
+          },
+        }
       );
 
       if (!updated || updated.matchedCount < 1) {
@@ -25,10 +25,7 @@ const details = new Elysia()
 
       return {
         success: true,
-        data: {
-          ...activity,
-          ...updateData,
-        },
+        message: "활동(동아리) 정보 수정 성공",
       };
     },
     {
@@ -56,19 +53,21 @@ const details = new Elysia()
       response: {
         200: t.Object({
           success: t.Boolean({
-            description: "수정 성공 여부",
+            description: "활동(동아리) 정보 수정 성공 여부",
             examples: [true],
           }),
-          data: activityElysiaSchema,
+          message: t.String({
+            description: "활동(동아리) 정보 수정 성공 메시지",
+            examples: ["활동(동아리) 정보 수정 성공"],
+          }),
         }),
-        ...errorElysia(["UPDATE_FAILED"]),
       },
       detail: {
         tags: ["Activity"],
-        summary: "활동(동아리) 상세 정보 수정",
-        description: "활동(동아리) 상세 정보(지원서 질문 목록, 모집 기간 등)를 수정합니다. 활동(동아리) 회장/부회장만 접근 가능합니다.",
-      },
-    }
+        summary: "활동(동아리) 세부 정보 수정하기",
+        description: "1개의 활동(동아리)의 정보를 수정합니다.",
+      }
+    },
   );
 
 export default details;
